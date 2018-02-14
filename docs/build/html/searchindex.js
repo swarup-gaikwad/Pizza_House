c5073e1b7fac500f69f944589dc0b1044c064e99
swarup-gaikwad <gaikwad.swarup54@gmail.com>
2018-02-14 17:21:09 +0530

    read the docs

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to sample docs&#8217;s documentation!"],titleterms:{doc:0,document:0,indice:0,sampl:0,tabl:0,welcom:0}})
